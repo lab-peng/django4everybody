3c5bb6cbba9a8edd735b86b6c4fd50b53088e690
Robert Pane <panerobert@yahoo.com>
2020-11-16 08:23:58 +0800

    morning

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world")
+
+
+def owner(request):
+    return HttpResponse("Hello, world. 27d52bcb is the polls index.")
